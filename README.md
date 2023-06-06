50a22fe667f682cedbab20565a8a39a4b5a27c0c
kingpin <monique.ahlheim@live.com>
2023-06-06 19:03:26 -0400

    updated javascript and README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# literate-tribble
-Module 3 JavaScript Challenge: Password Generator
+# Random Password Generator
+
+## Description
+
+This is a random password generator that will create a password for the user that can consist of at least one of the following: lowercase letters, uppercase letters, numbers, and special characters. The user will be prompted to make a choice about which characters are included. This generator solves the problem of having to generate a random password on your own.
+
+## Installation
+
+N/A
+
+## Usage
+
+Navigate to the website, click on the large red button that reads "Generate Password" and answer the window prompts as they appear. A randomly generated password will populate in the white text box in the middle of the page. 
+
+## License 
+
+Please refer to the license in the repo.
---
+++ assets/script.js
@@ -0,0 +1,65 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var body = document.querySelector("Your Secure Password");
+var passwordText = document.querySelector("#password");
+
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialChar = ["!", "#", "$", "%", "&", "*", "+"];
+
+// Write password to the #password input
+function generatePassword(event) {
+  var lowercaseQuestion = confirm("Would you like to use lowercase characters?");
+  var indexL = (Math.floor(Math.random() * lowercase.length));
+  var indexL2 = (Math.floor(Math.random() * lowercase.length));
+  if (lowercaseQuestion) {
+    for(var i = 0; i < lowercase.length; i++) {
+      var lowercaseChoice = lowercase[indexL] + lowercase[indexL2];
+    }
+  } else {
+    var lowercaseChoice = "";
+  }
+  
+  var uppercaseQuestion = confirm("Would you like to use uppercase characters?");
+  var indexU = Math.floor(Math.random() * uppercase.length);
+  var indexU2 = Math.floor(Math.random() * uppercase.length);
+  if (uppercaseQuestion) {
+    for(var i = 0; i < uppercase.length; i++) {
+    var uppercaseChoice = uppercase[indexU] + uppercase[indexU2];
+    }
+  } else {
+    var uppercaseChoice = "";
+  }
+
+  var numbersQuestion = confirm("Would you like to use numbers?");
+  var indexN = Math.floor(Math.random() * numbers.length);
+  var indexN2 = Math.floor(Math.random() * numbers.length);
+  if (numbersQuestion) {
+    for(var i = 0; i < numbers.length; i++) {
+    var numbersChoice = numbers[indexN] + numbers[indexN2];
+    }
+  } else {
+    numbersChoice = "";
+  }
+  
+  var specialCharQuestion = confirm("Would you like to use special characters?");
+  var indexSP = Math.floor(Math.random() * specialChar.length);
+  var indexSP2 = Math.floor(Math.random() * specialChar.length);
+  if (specialCharQuestion) {
+    for(var i = 0; i < specialChar.length; i++) {
+      var specialCharChoice = specialChar[indexSP] + specialChar[indexSP2];
+    }
+  } else {
+    specialCharChoice = "You must choose at least one character type! Refresh the page and try again!";
+  }
+
+  var password = lowercaseChoice + uppercaseChoice + numbersChoice + specialCharChoice; 
+  passwordText.textContent = password;
+
+}
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
